b745d06fe54f9aaf9f51bccb7c250c247ede47c4
Théo <t.meurier13@gmail.com>
2025-03-20 09:29:23 +0100

    Test de la protection de la branche main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./utils/styles/theme";
-import Dashboard from "./MainView/5-panels/Dashboard";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
     </ThemeProvider>
   );
 }
